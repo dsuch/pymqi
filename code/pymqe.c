7eaa65b0f846156b7d5db6dcf0dbcbed2e06d6e9
Matthew Wallace <matthew.wallace@redlinetrading.com>
2015-08-19 17:21:41 -0400

    Adding support for basic MQ 8.02 functionality

--- code/pymqe.c
+++ code/pymqe.c
@@ -1564,11 +1564,8 @@ __declspec(dllexport)
 #ifdef MQCMDL_LEVEL_750
       PyList_Append(versions, PyString_FromString("7.5"));
 #endif
-#ifdef MQCMDL_LEVEL_710
-      PyList_Append(versions, PyString_FromString("7.1"));
-#endif
-#ifdef MQCMDL_LEVEL_750
-      PyList_Append(versions, PyString_FromString("7.5"));
+#ifdef MQCMDL_LEVEL_800
+      PyList_Append(versions, PyString_FromString("8.0"));
 #endif
       PyDict_SetItemString(d,"__mqlevels__", PyList_AsTuple(versions));
       Py_XDECREF(versions);
