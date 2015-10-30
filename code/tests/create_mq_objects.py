44d6e1df46ccdbf0ef5b438e99c5e7085e51c324
hjoukl <holger.joukl@gmx.de>
2015-10-30 16:52:07 +0100

    Enhanced config and test object creation, extensive QueueManager unittests.
    This adds configuration control for the MQ8 connection authentication feature,
    both for use in test object creation in create_mq_objects.py and for test behaviour,
    i.e. depending on the setting some tests need to be skipped (e.g. if a queue 
    manager requires a user/password connection authentication a test for a method
    that won't provide these credentials can obviously not work).
    
    Care has been taken that the test suite can be run both against MQ 8 queue managers
    as well as pre-MQ 8 versions; some config changes from the defaults may be necessary.
    
    The pymqi.QueueManager class gets extensive unittest coverage.

--- code/tests/create_mq_objects.py
+++ code/tests/create_mq_objects.py
@@ -12,15 +12,73 @@ import time
 import config
 
 
-mqsc_script = """
+LINE = "-" * 80 + "\n"
+
+
+# channel and tcp listener creation commands
+mqsc_script_channel_listener = """
 DEFINE CHL(%(channel)s) CHLTYPE(SVRCONN)
 DEFINE LISTENER(TCP.LISTENER.1) TRPTYPE(TCP) PORT(%(port)s) CONTROL(QMGR) REPLACE
 START LISTENER(TCP.LISTENER.1)
 """ % {
     'channel': config.MQ.QM.CHANNEL,
     'port': config.MQ.QM.PORT,
-    } + '\n'.join([ "DEFINE QL(%s) REPLACE" % qname 
+    } + '\n'.join([ "DEFINE QL(%s) REPLACE" % qname
                     for qname in config.MQ.QUEUE.QUEUE_NAMES.values() ])
+
+
+# user/password connection authentication setup (MQ>=8.0 only) 
+# CHK*(OPTIONAL): If a user ID and password are provided by a client
+# application then they must be a valid pair. It is not mandatory to provide
+# user + password, though.
+# This setting allows for testing against both MQ>=8.0 servers as well as older
+# MQ versions with same test codebase.
+mqsc_script_conn_auth_use_pw = """
+ALTER QMGR CONNAUTH(USE.PW)
+DEFINE AUTHINFO(USE.PW) AUTHTYPE(IDPWOS) FAILDLAY(%(faildlay)s) CHCKLOCL(OPTIONAL) CHCKCLNT(%(chckclnt)s)
+REFRESH SECURITY TYPE(CONNAUTH)
+""" % {
+    'chckclnt': config.MQ.QM.CONN_AUTH.USE_PW,
+    'faildlay': config.MQ.QM.CONN_AUTH.FAIL_DELAY,
+    }
+
+
+mqsc_script_no_conn_auth = """
+ALTER QMGR CONNAUTH(' ')
+REFRESH SECURITY TYPE(CONNAUTH)
+"""
+
+
+# Disable channel auth records feature.
+# WARNING: Admin client connections will now be accepted per default - use for
+# testing purposes only!
+mqsc_script_channel_auth = """
+ALTER QMGR CHLAUTH(DISABLED)
+"""
+
+
+def run_mqsc(mqsc_script, errormsg="MQSC commands not successful",
+             verbose=True):
+    """Helper function to run MQSC commands.
+
+    Returns a (returncode, stdout, stderr)-tuple of the command script.
+    """
+    if verbose:
+        print "runmqsc:"
+        print mqsc_script.replace('\n', '\n    ')
+    runmqsc_proc = subprocess.Popen(
+        ["runmqsc", config.MQ.QM.NAME], stdout=subprocess.PIPE,
+        stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    runmqsc_output, runmqsc_error = runmqsc_proc.communicate(mqsc_script)
+    print LINE
+    print runmqsc_output
+    print LINE
+    if runmqsc_proc.returncode not in (0, 10):
+        print errormsg
+        print LINE
+        print runmqsc_error
+        print LINE
+    return runmqsc_proc.returncode, runmqsc_output, runmqsc_error
 
 
 print "Creating all MQ Objects required to run the test.\n"
@@ -78,18 +136,29 @@ else:
 
 time.sleep(2)
 
-print "Creating MQ Objects."
-runmqsc_proc = subprocess.Popen(["runmqsc", config.MQ.QM.NAME],
-                                stdout=subprocess.PIPE, stdin=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-runmqsc_output, runmqsc_error = runmqsc_proc.communicate(mqsc_script)
-print "-" * 80 + "\n"
-print runmqsc_output
-print "-" * 80 + "\n"
-if runmqsc_proc.returncode not in (0, 10):
-    print "Creation of MQ Objects not successful."
-    print "-" * 80 + "\n"
-    print runmqsc_error
-    print "-" * 80 + "\n"
+print "Creating MQ channel and listener Objects."
+run_mqsc(mqsc_script_channel_listener,
+         errormsg="MQ channel and listener objects creation not successful.")
+
+if config.MQ.QM.CONN_AUTH.SUPPORTED == '1':
+    print "Setting up MQ queue manager user/password connection authentication."
+    run_mqsc(mqsc_script_conn_auth_use_pw,
+         errormsg="MQ queue manager connection authentication setup not "
+         "successful.")
+else:
+    if int(config.MQ.QM.MIN_COMMAND_LEVEL) >= 800:
+        print "Switching off MQ queue manager connection authentication."
+        run_mqsc(mqsc_script_no_conn_auth,
+            errormsg="MQ queue manager connection authentication setup not "
+            "successful.")
+    else:
+        print "Connection authentication not applicable for pre-8.0 MQ."
+        
+    
+print "Disabling MQ queue manager channel authentication records feature."
+run_mqsc(mqsc_script_channel_auth,
+         errormsg="Disabling MQ queue manager channel authentication records "
+         "not successful.")
+
 
 print "MQ Environment Created.  Ready for tests.\n"
