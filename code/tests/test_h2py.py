d606ed3107e7ecd497c648e88488f6949dbc6149
Seyf SV <25712100+SeyfSV@users.noreply.github.com>
2020-02-01 11:09:26 +0300

    GH #158 Test_h2py fixed

--- code/tests/test_h2py.py
+++ code/tests/test_h2py.py
@@ -3,42 +3,65 @@ unwanted duplicate entries.
 """
 
 # stdlib
-import sys
 import inspect
 import unittest
-
-# test env settings
-import env
+from struct import calcsize
 
 # PyMQI
 from pymqi import CMQC, CMQXC, CMQCFC
 
-ignore_dups = ["MQOD_CURRENT_LENGTH", "MQPMO_CURRENT_LENGTH", "MQRC_NONE",
-               "MQACH_CURRENT_LENGTH", "MQCD_CURRENT_LENGTH", "MQCD_LENGTH_4",
-               "MQCD_LENGTH_5", "MQCD_LENGTH_6", "MQCD_LENGTH_7", "MQCD_LENGTH_8",
-               "MQCD_LENGTH_9", "MQCD_LENGTH_10", "MQCD_LENGTH_11",
-               "MQCFIF_STRUC_LENGTH", "MQCFGR_STRUC_LENGTH",
-               "MQCFIN64_STRUC_LENGTH", "MQCFIN_STRUC_LENGTH", "MQACH_LENGTH_1"]
+ignore_dups = []
 
 class Testh2py(unittest.TestCase):
+    """ Test that modules generated automatically by h2py have no unwanted duplicate entries.
+    """
 
     def test_h2py(self):
         """ Test that modules generated automatically by h2py have no unwanted duplicate entries.
         """
 
         errors = []
+        test_passed = False
 
         for mod in CMQC, CMQXC, CMQCFC:
             mq_attrs = []
             mq_attrs_count = {}
-            dups = {}
+
             source_lines = inspect.getsourcelines(mod)
+            if mod == CMQC:
+                source_lines[0].append('MQ_DUMMY_1 = 1')
+                source_lines[0].append('MQ_DUMMY_1 = 1')
+                source_lines[0].append('if calcsize("P") == 8:')
+                source_lines[0].append('    MQ_DUMMY_2 = 1')
+                source_lines[0].append('else:')
+                source_lines[0].append('    MQ_DUMMY_2 = 1')
+            process = True
+            in_if = False
+            indent = 0
             for line in source_lines[0]:
+                len_line = len(line)
+                len_line_lstrip = len(line.lstrip(' '))
+                line = line.strip()
+
                 if line:
-                    line = line.strip()
-                    if line.startswith("MQ"):
-                        mq_attr = line.split()[0].split(",")[0]
-                        mq_attrs.append(mq_attr)
+                    if "".join(line.split()) == ''.join('if calcsize("P") == 8:'.split()):
+                        in_if = True
+                        process = calcsize("P") == 8
+                    elif "".join(line.split()) == 'else:' and in_if:
+                        process = not process
+                    else:
+                        if in_if and not indent:
+                            indent = len_line - len_line_lstrip
+                        elif (len_line - len_line_lstrip) < indent and line[0] != '#':
+                            in_if = False
+                            process = True
+                            indent = 0
+
+                        if line.startswith("MQ") and process:
+                            line_split = line.split()
+                            mq_attr = line_split[0].split(",")[0]
+                            if len(line_split) > 1 and line_split[1] == '=':
+                                mq_attrs.append(mq_attr)
 
             for mq_attr in mq_attrs:
                 if mq_attr not in mq_attrs_count:
@@ -49,6 +72,8 @@ class Testh2py(unittest.TestCase):
             for mq_attr, count in mq_attrs_count.items():
                 if count == 1 or(count == 2 and mq_attr in ignore_dups):
                     continue
+                elif mq_attr.startswith('MQ_DUMMY'):
+                    test_passed = True
                 else:
                     item = "%s is defined %d times in %s" % (mq_attr, count, mod)
                     errors.append(item)
@@ -56,6 +81,8 @@ class Testh2py(unittest.TestCase):
         if errors:
             msg = "\n" + "\n".join(errors)
             self.fail(msg)
+        
+        self.assertTrue(test_passed, 'Test does not found DUMMY duplicate entries!')
 
 if __name__ == "__main__":
     unittest.main()
