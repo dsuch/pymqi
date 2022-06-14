30c947a3c9c116b9919e05a23addf45a3b436b1c
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-06-14 18:16:35 +0200

    Releasing 1.12.3.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from distutils.core import Extension
 from distutils import spawn
 from struct import calcsize
 
-version = '1.12.1'
+version = '1.12.2'
 
 # Build either in bindings or client mode.
 bindings_mode = 0
@@ -180,6 +180,62 @@ else:
     print('Building PyMQI client mode %sbits' % bits)
 
 print('Using library_dirs:`%s`, include:`%s`, libraries:`%s`' % (library_dirs, include_dirs, libraries))
+
+long_description = """
+# Python library for IBM MQ
+
+PyMQI is a production-ready, open-source Python extension for IBM MQ (formerly known as WebSphere MQ and MQSeries).
+
+For 20+ years, the library has been used by thousands of companies around the world with their queue managers running on
+Linux, Windows, UNIX and z/OS.
+
+## Sample code
+
+To put a message on a queue:
+
+```python
+import pymqi
+
+queue_manager = pymqi.connect('QM.1', 'SVRCONN.CHANNEL.1', '192.168.1.121(1434)')
+
+q = pymqi.Queue(queue_manager, 'TESTQ.1')
+q.put('Hello from Python!')
+```
+
+To read the message back from the queue:
+
+```python
+import pymqi
+
+queue_manager = pymqi.connect('QM.1', 'SVRCONN.CHANNEL.1', '192.168.1.121(1434)')
+
+q = pymqi.Queue(queue_manager, 'TESTQ.1')
+msg = q.get()
+print('Here is the message:', msg)
+```
+
+## The backbone of IBM MQ Python messaging
+
+PyMQI is a low-level library that requires one to know IBM MQ APIs well.
+
+It serves, however, as the basis for
+IBM MQ
+support
+in
+[Zato](https://zato.io/?gh),
+which is an enterprise
+API platform and backend application server in Python that lets one connect to many technologies
+with little or no programming.
+
+This includes IBM MQ queue managers as well as the ability to seamlessly integrate with Java JMS systems.
+
+![](https://zato.io/en/docs/3.2/gfx/api/screenshots/conn1.png?gh)
+![](https://zato.io/en/docs/3.2/gfx/api/screenshots/mq.png?gh)
+
+## Learn more
+
+Visit the [documentation](https://zato.io/en/docs/3.2/pymqi/index.html?gh) for more information and usage examples.
+"""
 
 setup(name = 'pymqi',
     version = version,
